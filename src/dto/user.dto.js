ab1b5a3400b17f0ae3f38e6bf5627e26cd78e8a1
SergeyGSA <goreslavskiy.sergey@gmail.com>
2022-07-22 10:53:18 +0300

    changed user-dto

--- src/dto/user.dto.js
+++ src/dto/user.dto.js
@@ -1,9 +1,13 @@
 export class UserDto {
   email
   id
+  fullName
+  avatarUrl
 
   constructor(model) {
     this.email = model.email
     this.id = model._id
+    this.fullName = model.fullName
+    this.avatarUrl = model?.avatarUrl
   }
 }
